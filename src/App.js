883f68f64b4f010431fbde9ea323870ece181a1a
AliEtehadianMoghadam <95126169+AliEtehadianMoghadam@users.noreply.github.com>
2022-03-14 08:43:20 +0330

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Porfile from './PortofiloContainer/Home/Porfile'
+
+function App () {
+  return (
+    <div className='App'>
+      <Porfile/>
+    </div>
+  )
+}
+
+export default App
